adfb19c992cc7053beb36dbc3c111ad381727ef7
Pritom <pritomrazratulpaul@gmail.com>
2026-03-16 22:40:51 +0600

    JS Complete

--- Lab Performance_MID/JS/Validation.js
+++ Lab Performance_MID/JS/Validation.js
@@ -1,1 +1,34 @@
 console.log("HTML Page are Connected"); 
+
+const unitPrice = 1000;
+ 
+const quantityInputperday = document.getElementById("quantity");
+const totalPriceInput = document.getElementById("totalPrice");
+ 
+function calculateTotal(){
+ 
+let quantity = parseInt(quantityInputperday.value) || 0;
+ 
+if(quantity < 0){
+    alert("Quantity cannot be negative. Resetting to 0.");
+    quantity = 0;
+    quantityInputperday.value = 0;
+}
+ 
+let total = unitPrice * quantity * 30;
+ 
+totalPriceInput.value = total;
+ 
+if(total > 1000){
+    alert("Congratulations! You are eligible for a gift coupon.");
+}
+
+if(quantity = 0){
+    alert("Error Messege");
+    quantity = 0;
+    quantityInputperday.value = 0;
+}
+ 
+}
+quantityInputperday.addEventListener("input", calculateTotal);
+ 
