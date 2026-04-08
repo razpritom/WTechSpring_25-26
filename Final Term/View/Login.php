7a53df2e10a9f8a87468fb8d2ddd5614c348d1d1
Pritom <pritomrazratulpaul@gmail.com>
2026-04-08 11:25:05 +0600

    XAMP Connected

---
+++ Final Term/View/Login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <title>User Information</title>
+        <form>
+            <table>
+                <tr>
+                    <td>
+                        <p>User Name : </p>
+                        <input type ="text"/>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </body>    
+    
+</html>
